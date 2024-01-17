a51ee85f601417432a33198434abb9b2011c93bd
Sean Marquez <capsulecorplab@gmail.com>
2024-01-17 00:51:21 +0000

    implement schedIn_handler in MathReceiver.cpp

--- Components/MathReceiver/MathReceiver.cpp
+++ Components/MathReceiver/MathReceiver.cpp
@@ -80,11 +80,14 @@ namespace MathModule {
 
   void MathReceiver ::
     schedIn_handler(
-        NATIVE_INT_TYPE portNum,
+        const NATIVE_INT_TYPE portNum,
         NATIVE_UINT_TYPE context
     )
   {
-    // TODO
+    U32 numMsgs = this->m_queue.getNumMsgs();
+    for (U32 i = 0; i < numMsgs; ++i) {
+        (void) this->doDispatch();
+    }
   }
 
   // ----------------------------------------------------------------------
